40b8fcbbe4b6f4a5d7fb999a1273c3678810de5e
soli <liuwanli@taihe.com>
2020-07-24 18:00:59 +0800

    日志熟悉

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,11 +12,20 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 @SpringBootApplication
 @EnableSwagger2WebMvc
 @ComponentScan("com.example.demo")
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+    @Value("${file.setting.port}")
+    private String settingVale;
 
     //	http://localhost:8080/swagger-ui.html
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        logger.debug("获取到的值：" + settingVale);
+    }
 }
